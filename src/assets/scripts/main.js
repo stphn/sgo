f5eb970bc23ae7c8272fb6ef2002e9960aecca4b
Stéphane Goeuriot <sgoeuriot@comwrap.com>
2016-11-08 06:30:30 +0100

    Added Branding in console

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,3 +3,4 @@
 import './_objectassign'
 import './_dribbble'
 import './_anim'
+console.log('%c www.setgraphic.com', 'padding: 11px 8px 11px 5px; color:#fff; background-color:#292929; line-height:25px')
